e296596b24b97de7a18efaf920dc7b0be51082e7
Rémi Joussé <remi.jousse@ext.adeo.com>
2025-01-18 14:04:05 +0100

    First works on addon service

--- addon.py
+++ addon.py
@@ -1,56 +1,54 @@
-import time
+from resources.lib.service import SkippTvService
 import xbmc
 
-class SkippTv(xbmc.Player):
-    def __init__(self):
-        print("Launch skipp")
-        super(SkippTv, self).__init__()
-
-    def onPlayBackStarted(self):
-        # Triggered when playback starts
-        print("Hey SKIPP TV IS ON !!")
-        self.get_video_infos()
-        # if self.check_conditions(video_info):
-        #     self.show_skip_button()isPlayingVideo
-
-    def get_video_infos(self):
-        print("Je passe dans la fonction")
-        print(self.isPlayingVideo())
-        print(xbmc.Player().isPlayingVideo())
-        # Retrieve current playing video information
-        # if self.isPlayingVideo():
-        video_info = self.getVideoInfoTag()
-        print("slkdslkfjdskfjdslkj")
-        print(video_info.__dict__)
-            
-    # def check_conditions(self, video_info):
-    #     # Add logic to check tags/conditions
-    #     return "specific_tag" in video_info
-
-    # def show_skip_button(self):
-    #     dialog = xbmcgui.Dialog()
-    #     if dialog.yesno("Skip Section", "Do you want to skip this section?"):
-    #         self.skip_to(300)  # Skip to 5 minutes (300 seconds)
-
-    # def skip_to(self, time):
-    #     self.player.seekTime(time)
-
-skippTv = SkippTv()
-
 if __name__ == '__main__':
+    skippTvService = SkippTvService()
     monitor = xbmc.Monitor()
-    
+
+
     while not monitor.abortRequested():
-        # Sleep/wait for abort for 10 seconds
-        if monitor.waitForAbort(10):
-            # Abort was requested while waiting. We should exit
-            break
-        if xbmc.Player().isPlayingVideo():
-            print("hehehehehe playing video !")
-            print(xbmc.Player().getVideoInfoTag().getTagLine())
-            print(xbmc.Player().getVideoInfoTag().getDbId())
-            print(xbmc.Player().getVideoInfoTag().getGenre())
-            print(xbmc.Player().getVideoInfoTag().getIMDBNumber())
-            print(xbmc.Player().getVideoInfoTag().getSeason())
-            print(xbmc.Player().getVideoInfoTag().getEpisode())
-        xbmc.log("hello addon! %s" % time.time(), level=xbmc.LOGDEBUG)
+            # Sleep/wait for abort for 10 seconds
+            if monitor.waitForAbort(10):
+                # Abort was requested while waiting. We should exit
+                break
+            
+            # Vérifie si un événement de lecture commence
+            player = xbmc.Player()
+            if player.isPlaying():
+                print("Playback detected. Waiting for media info...")
+
+                try:
+                    # Récupère les informations sur le média
+                    media_type = player.getVideoInfoTag().getMediaType()
+                    print(f"Media type detected: {media_type}")
+
+                    if media_type == "episode":
+                        # Récupère les informations détaillées sur la vidéo
+                        video_info = player.getVideoInfoTag()
+                        season = video_info.getSeason()
+                        episode = video_info.getEpisode()
+                        tmdbid = xbmc.Player().getPlayingItem().getProperty("TmdbId") or "-1"
+
+                        if season is not None and episode is not None:
+                            title = video_info.getTVShowTitle()
+                            if title is not None:
+                                final_endpoint = (
+                                    f"https://skipptv.nastream.fr/byTitle/{title.lower().replace(' ', '_')}_"
+                                    f"{str(season).zfill(2)}_{str(episode).zfill(2)}"
+                                )
+                                if tmdbid != "-1":
+                                    final_endpoint = (
+                                        f"https://skipptv.nastream.fr/byTmdbIdAndDatas/{tmdbid}/"
+                                        f"{season}/{episode}"
+                                    )
+                                skippTvService.log(f"Final endpoint: {final_endpoint}")
+                                # self.send_notification("Final endpoint", final_endpoint)
+                            else:
+                                skippTvService.log("Impossible de récupérer le titre de la série")
+                        else:
+                            skippTvService.log("Impossible de récupérer les détails de l'épisode.")
+                except Exception as e:
+                    skippTvService.log(f"Erreur en récupérant les informations vidéo : {str(e)}")
+                while player.isPlaying():
+                    xbmc.sleep(500)
+
--- resources/lib/service.py
+++ resources/lib/service.py
@@ -1,35 +1,10 @@
+import time
 import xbmc
-import xbmcgui
-import xbmcplugin
+import xbmcaddon
 
-class SkippTv(xbmc.Monitor):
+class SkippTvService:
     def __init__(self):
-        print("Launch skipp")
-        super().__init__()
-        self.player = xbmc.Player()
+        print("Initialisation du service SkippTv")
 
-    def onPlayBackStarted(self):
-        # Triggered when playback starts
-        print("Hey SKIPP TV IS ON !!")
-        # video_info = self.get_video_info()
-        # if self.check_conditions(video_info):
-        #     self.show_skip_button()
-
-    # def get_video_info(self):
-    #     # Retrieve current playing video information
-    #     if self.player.isPlayingVideo():
-    #         video_file = self.player.getPlayingFile()
-    #         return xbmc.getInfoLabel('ListItem.Property(tagline)')  # Example of fetching tags
-    #     return None
-
-    # def check_conditions(self, video_info):
-    #     # Add logic to check tags/conditions
-    #     return "specific_tag" in video_info
-
-    # def show_skip_button(self):
-    #     dialog = xbmcgui.Dialog()
-    #     if dialog.yesno("Skip Section", "Do you want to skip this section?"):
-    #         self.skip_to(300)  # Skip to 5 minutes (300 seconds)
-
-    # def skip_to(self, time):
-    #     self.player.seekTime(time)
+    def log(self, message):
+        xbmc.log(f"[SkippTV] {message}", level=xbmc.LOGINFO)
